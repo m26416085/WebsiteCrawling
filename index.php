a9d52aa58eed5fc106cf6ed6c3296f7d7e2598fa
m26416085 <37286160+m26416085@users.noreply.github.com>
2019-07-09 13:55:38 +0700

    Key stored to array

--- index.php
+++ index.php
@@ -1,74 +1,92 @@
 <?php
 
-function http_request($url){
+function http_request($url)
+{
     // persiapkan curl
-    $ch = curl_init(); 
+    $ch = curl_init();
 
     // set url 
     curl_setopt($ch, CURLOPT_URL, $url);
-    
+
     // set user agent    
-    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 
     // return the transfer as a string 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
     // $output contains the output string 
-    $output = curl_exec($ch); 
+    $output = curl_exec($ch);
 
     // tutup curl 
-    curl_close($ch);   
+    curl_close($ch);
 
     // mengembalikan hasil curl
     return $output;
 }
-
-if (isset($_POST['find']))
-	{
-        $search = $_POST['text_value'];
-
-        $search = str_replace(' ', '%20', $search);
-
-        $url = "https://ta.tokopedia.com/promo/v1/display/ads?user_id=0&ep=product&item=20&src=search&device=desktop&page=2&q=".$search."&fshop=1";
-
-        $profile = http_request($url);
-
-        // ubah string JSON menjadi array
-        $profile = json_decode($profile, TRUE);
-
-        #echo $profile["data"][0]["product"]["name"];
-        $i = 0;
-        foreach($profile["data"] as $profil){
-            echo '<img src='.$profile["data"][$i]["product"]["image"]["m_url"].'><br>';
-            echo $profile["data"][$i]["product"]["name"].'<br>';
-            echo $profile["data"][$i]["product"]["price_format"].'<br>';
-            echo $profile["data"][$i]["shop"]["name"].'<br>';
-            echo $profile["data"][$i]["shop"]["location"].'<br>';
-            echo '<a href='.$profile["data"][$i]["product"]["uri"].'>Link</a>'.'<br>';
-            $i = $i + 1;
-        }
-    }
-
-
-
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Curl Data JSON</title>
 </head>
-<body>
-<form action = "index.php" method = "POST">
-    <input type = "text" name="text_value"><input type = "submit" value = "search" name = "find">
-</form>
-<!-- <img src="<?php echo $profile['avatar_url']; ?>" width="64" /> -->
-<br>
-<p>
-<!-- Nama: <?php echo $profile["data"][0]["product"]["name"] ?><br>
-URL: <a href="<?php echo $profile["uri"] ?>"><?php echo $profile["uri"] ?></a><br>
-Lokasi: <?php echo $profile["location"] ?> -->
-</p>
 
+<body>
+    <?php
+        session_start();
+        if (!isset($_SESSION['cart_item'])) {
+            $_SESSION['cart_item'] = array();
+            echo "masuk create";
+        }
+        else{
+            // array_push($_SESSION['cart_item'], $_GET['id_product']);
+            array_push($_SESSION['cart_item'], $i);
+            print_r($_SESSION['cart_item']);
+            echo "bukan create";
+        }
+    ?>
+    <form action="index.php" method="POST">
+        <input type="text" name="text_value"><input type="submit" value="search" name="find">
+    </form>
 </body>
 </html>
+
+<?php
+    error_reporting(0);
+    if ($_GET['search'] == null){
+        $search = $_POST['text_value'];
+    }
+    else{
+        echo $search;
+        $search = $_GET['search'];
+    }
+
+    $search = str_replace(' ', '%20', $search);
+
+    $url = "https://ta.tokopedia.com/promo/v1/display/ads?user_id=0&ep=product&item=20&src=search&device=desktop&page=2&q=" . $search . "&fshop=1";
+
+    $data = http_request($url);
+
+    // ubah string JSON menjadi array
+    $data = json_decode($data, TRUE);
+
+    //add from json array to php array
+    $x = 0;
+    // foreach($data["data"] as $php_array){
+    //     $php_array[]
+    // }
+
+    #echo $profile["data"][0]["product"]["name"];
+    $i = 0;
+    foreach ($data["data"] as $profil) {
+        echo '<img src=' . $data["data"][$i]["product"]["image"]["m_url"] . '><br>';
+        echo $data["data"][$i]["product"]["name"] . '<br>';
+        echo $data["data"][$i]["product"]["price_format"] . '<br>';
+        echo $data["data"][$i]["shop"]["name"] . '<br>';
+        echo $data["data"][$i]["shop"]["location"] . '<br>';
+        echo '<a href=' . $data["data"][$i]["product"]["uri"] . '>Link</a>' . '<br>';
+        echo '<a href="index.php?id_product=' . $data["data"][$i]["id"] . '&search='.$search.'" id="addtocart">Add to Cart</a><br><br>';
+        $i = $i + 1;
+    }
+?>
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+    echo "masuk";
+    echo $_GET["id"];
+?>
